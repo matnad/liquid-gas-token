017f0d311e249d7405a3431023452b84adfaac84
Matthias Nadler <matnad@gmail.com>
2020-06-07 11:30:22 +0200

    feat: automate benchmarks

--- tests/benchmark/test_bm_free.py
+++ tests/benchmark/test_bm_free.py
@@ -1,18 +1,53 @@
+import json
+from pathlib import Path
+import inspect
+
 import pytest
 from brownie import *
 from brownie.utils import color
 
 
 DEADLINE = 999999999999999
+UPDATE_BENCHMARKS = False
 
 
-def print_benchmark(used, benchmark):
+def process_benchmark(used, benchmark, obj):
     if used == benchmark:
         print(f"[ {color('bright yellow')}BENCHMARK MATCHED {color}]")
     elif used < benchmark:
+        save_benchmark_to_file(inspect.stack()[1].function, obj, used)
         print(f"[ {color('dark green')}BENCHMARK BEATEN BY {benchmark-used}{color} ]")
     else:
         print(f"[ {color('bright red')}BENCHMARK MISSED BY {used-benchmark}{color} ]")
+
+
+def params_from_file():
+    with Path(__file__).parent.absolute().joinpath(Path("benchmarks.json")).open() as fp:
+        benchmarks = json.load(fp)
+    return benchmarks
+
+
+PARAMS = params_from_file()
+
+
+def save_benchmark_to_file(function, obj, new_benchmark):
+    if not UPDATE_BENCHMARKS:
+        return
+    benchmarks = params_from_file()
+    objs = benchmarks.setdefault(function, [])
+    updated_obj = obj.copy()
+    updated_obj["gas"] = new_benchmark
+    found = False
+    for i, entry in enumerate(objs):
+        if entry["burn"] == obj["burn"] and entry["free"] == obj["free"]:
+            objs[i] = updated_obj
+            found = True
+            break
+    if not found:
+        objs.append(updated_obj)
+
+    with Path(__file__).parent.absolute().joinpath(Path("benchmarks.json")).open("w") as fp:
+        json.dump(benchmarks, fp)
 
 
 def test_setup(liquid_lgt, accounts):
@@ -23,55 +58,34 @@ def test_setup(liquid_lgt, accounts):
     assert liquid_lgt.poolTokenReserves() == 92
 
 
-@pytest.mark.parametrize(
-    "params", [
-        {'burn': 1e5, 'free': 4, 'gas': 86623},
-        {'burn': 5e5, 'free': 12, 'gas': 332072},
-        {'burn': 1e6, 'free': 25, 'gas': 598102},
-        {'burn': 2e6, 'free': 50, 'gas': 1171290},
-    ]
-)
+@pytest.mark.parametrize("params", PARAMS['test_burn_and_free_from'])
 def test_burn_and_free_from(liquid_lgt, helper, accounts, params):
     liquid_lgt.mint(params['free'], {'from': accounts[0]})
     tx_free = helper.burnAndFree(params['burn'], params['free'], {'from': accounts[0]})
     gas_paid = tx_free.gas_used
-    print_benchmark(gas_paid, params['gas'])
+    process_benchmark(gas_paid, params['gas'], params)
     print(f"Gas Paid: {gas_paid}/{int(params['burn'])} with {params['free']} owned tokens.")
     assert gas_paid <= params["gas"]
 
 
-@pytest.mark.parametrize(
-    "params", [
-        {'burn': 1e5, 'free': 4, 'gas': 81582},
-        {'burn': 5e5, 'free': 12, 'gas': 321990},
-        {'burn': 1e6, 'free': 25, 'gas': 593061},
-        {'burn': 2e6, 'free': 50, 'gas': 1166249},
-    ]
-)
+@pytest.mark.parametrize("params", PARAMS['test_burn_buy_and_free_exact'])
 def test_burn_buy_and_free_exact(liquid_lgt, helper, accounts, params):
     initial_balance = accounts[0].balance()
     price = liquid_lgt.getEthToTokenOutputPrice(params['free'])
     tx_free = helper.burnBuyAndFree(params['burn'], params['free'], {'from': accounts[0], 'value': price})
     gas_paid = tx_free.gas_used
-    print_benchmark(gas_paid, params['gas'])
+    process_benchmark(gas_paid, params['gas'], params)
     print(f"Gas Paid: {gas_paid}/{int(params['burn'])} with {params['free']} bought tokens.")
     assert gas_paid <= params["gas"]
     assert accounts[0].balance() < initial_balance
 
 
-@pytest.mark.parametrize(
-    "params", [
-        {'burn': 1e5, 'free': 4, 'gas': 81461},
-        {'burn': 5e5, 'free': 12, 'gas': 321748},
-        {'burn': 1e6, 'free': 25, 'gas': 592940},
-        {'burn': 2e6, 'free': 50, 'gas': 1166128},
-    ]
-)
+@pytest.mark.parametrize("params", PARAMS['test_burn_buy_up_to_and_free'])
 def test_burn_buy_up_to_and_free(liquid_lgt, helper, accounts, params):
     initial_balance = accounts[0].balance()
     tx_free = helper.burnBuyUpToAndFree(params['burn'], params['free'], {'from': accounts[0], 'value': "5 ether"})
     gas_paid = tx_free.gas_used
-    print_benchmark(gas_paid, params['gas'])
+    process_benchmark(gas_paid, params['gas'], params)
     print(f"Gas Paid: {gas_paid}/{int(params['burn'])} with (up to) {params['free']} bought tokens.")
     assert gas_paid <= params["gas"]
     assert accounts[0].balance() < initial_balance
--- tests/benchmark/test_bm_mint.py
+++ tests/benchmark/test_bm_mint.py
@@ -1,17 +1,51 @@
+import inspect
+import json
+from pathlib import Path
+
 import pytest
-from brownie import *
 from brownie.utils import color
 
 DEADLINE = 999999999999999
+UPDATE_BENCHMARKS = False
 
 
-def print_benchmark(used, benchmark):
+def process_benchmark(used, benchmark, obj):
     if used == benchmark:
         print(f"[ {color('bright yellow')}BENCHMARK MATCHED {color}]")
     elif used < benchmark:
+        save_benchmark_to_file(inspect.stack()[1].function, obj, used)
         print(f"[ {color('dark green')}BENCHMARK BEATEN BY {benchmark-used}{color} ]")
     else:
         print(f"[ {color('bright red')}BENCHMARK MISSED BY {used-benchmark}{color} ]")
+
+
+def params_from_file():
+    with Path(__file__).parent.absolute().joinpath(Path("benchmarks.json")).open() as fp:
+        benchmarks = json.load(fp)
+    return benchmarks
+
+
+PARAMS = params_from_file()
+
+
+def save_benchmark_to_file(function, obj, new_benchmark):
+    if not UPDATE_BENCHMARKS:
+        return
+    benchmarks = params_from_file()
+    objs = benchmarks.setdefault(function, [])
+    updated_obj = obj.copy()
+    updated_obj["gas"] = new_benchmark
+    found = False
+    for i, entry in enumerate(objs):
+        if entry["amount"] == obj["amount"]:
+            objs[i] = updated_obj
+            found = True
+            break
+    if not found:
+        objs.append(updated_obj)
+
+    with Path(__file__).parent.absolute().joinpath(Path("benchmarks.json")).open("w") as fp:
+        json.dump(benchmarks, fp)
 
 
 def test_setup(liquid_lgt, accounts):
@@ -22,66 +56,38 @@ def test_setup(liquid_lgt, accounts):
     assert liquid_lgt.poolTokenReserves() == 92
 
 
-@pytest.mark.parametrize(
-    "params", [
-        {'amount': 1, 'gas': 75622},
-        {'amount': 10, 'gas': 404023},
-        {'amount': 32, 'gas': 1205390},
-        {'amount': 71, 'gas': 2627070}
-    ]
-)
+@pytest.mark.parametrize("params", PARAMS['test_mint'])
 def test_mint(liquid_lgt, accounts, params):
     tx_mint = liquid_lgt.mint(params["amount"], {'from': accounts[0]})
     gas_mint = tx_mint.gas_used
-    print_benchmark(gas_mint, params['gas'])
+    process_benchmark(gas_mint, params['gas'], params)
     print(f"Gas Paid: {gas_mint} for {params['amount']} tokens.")
     assert gas_mint <= params["gas"]
 
 
-@pytest.mark.parametrize(
-    "params", [
-        {'amount': 1, 'gas': 74669},
-        {'amount': 10, 'gas': 403070},
-        {'amount': 32, 'gas': 1204437},
-        {'amount': 71, 'gas': 2626117}
-    ]
-)
+@pytest.mark.parametrize("params", PARAMS['test_mint_to_sell'])
 def test_mint_to_sell(liquid_lgt, accounts, params):
     initial_balance = accounts[0].balance()
     tx_mint = liquid_lgt.mintToSell(params["amount"], 1, DEADLINE, {'from': accounts[0]})
     gas_mint = tx_mint.gas_used
-    print_benchmark(gas_mint, params['gas'])
+    process_benchmark(gas_mint, params['gas'], params)
     print(f"Gas Paid: {gas_mint}. Minted {params['amount']} tokens.")
     assert gas_mint <= params["gas"]
     assert accounts[0].balance() > initial_balance
 
 
-@pytest.mark.parametrize(
-    "params", [
-        {'amount': 1, 'gas': 75067},
-        {'amount': 10, 'gas': 403468},
-        {'amount': 32, 'gas': 1204835},
-        {'amount': 71, 'gas': 2626515}
-    ]
-)
+@pytest.mark.parametrize("params", PARAMS['test_mint_to_sell_to'])
 def test_mint_to_sell_to(liquid_lgt, accounts, params):
     initial_balance = accounts[1].balance()
     tx_mint = liquid_lgt.mintToSellTo(params["amount"], 1, DEADLINE, accounts[1], {'from': accounts[0]})
     gas_mint = tx_mint.gas_used
-    print_benchmark(gas_mint, params['gas'])
+    process_benchmark(gas_mint, params['gas'], params)
     print(f"Gas Paid: {gas_mint}. Minted and sold {params['amount']} tokens.")
     assert gas_mint <= params["gas"]
     assert accounts[1].balance() > initial_balance
 
 
-@pytest.mark.parametrize(
-    "params", [
-        {'amount': 1, 'eth': 5978260869565216, 'gas': 83204},
-        {'amount': 10, 'eth': 59782608695652172, 'gas': 411605},
-        {'amount': 32, 'eth': 191304347826086955, 'gas': 1212972},
-        {'amount': 71, 'eth': 424456521739130433, 'gas': 2634652}
-    ]
-)
+@pytest.mark.parametrize("params", PARAMS['test_mint_to_liquidity'])
 def test_mint_to_liquidity(liquid_lgt, accounts, params):
     initial_shares = liquid_lgt.poolBalanceOf(accounts[1])
     tx_mint = liquid_lgt.mintToLiquidity(
@@ -96,7 +102,7 @@ def test_mint_to_liquidity(liquid_lgt, accounts, params):
     assert tokens == params['amount']
     assert eth == params['eth']
     assert shares == params['eth']
-    print_benchmark(gas_mint, params['gas'])
+    process_benchmark(gas_mint, params['gas'], params)
     print(f"Gas Paid: {gas_mint}. Minted and added {params['amount']} tokens and {params['eth']} wei.")
     assert gas_mint <= params["gas"]
     assert liquid_lgt.poolBalanceOf(accounts[1]) > initial_shares
