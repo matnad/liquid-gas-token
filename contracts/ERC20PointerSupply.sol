8f6336b247bc362aaa64eeb1488c0eb4be841bfd
Matthias Nadler <matnad@gmail.com>
2020-06-09 17:14:12 +0200

    chore: add license

--- contracts/ERC20PointerSupply.sol
+++ contracts/ERC20PointerSupply.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.7;
 
 import "OpenZeppelin/openzeppelin-contracts@3.0.1/contracts/math/SafeMath.sol";
--- contracts/LiquidERC20.sol
+++ contracts/LiquidERC20.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.7;
 
 import "./ERC20PointerSupply.sol";
